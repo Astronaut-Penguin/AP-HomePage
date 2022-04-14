429dfba6c43762fdf673d5f56c9d20693553e846
FacundoRago <facundorago98@gmail.com>
2022-04-14 12:31:13 -0300

    New Footer Section created
    Yet to start building it

---
+++ src/sections/footer/footer.jsx
@@ -0,0 +1,13 @@
+//- Import React
+import React from 'react';
+
+//- Import Styles
+import styles from './footer.module.css';
+
+const FooterSection = () => {
+    return (
+        <div>Goli</div>
+    );
+}
+
+export default FooterSection;
