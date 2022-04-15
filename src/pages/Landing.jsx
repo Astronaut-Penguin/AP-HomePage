acc53a32871262e81e3066c0b988fb00f9ccd850
FacundoRago <facundorago98@gmail.com>
2022-04-15 13:35:58 -0300

    [New Project] - New Section ready
    Yet to improve Mobile Views

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,11 +4,14 @@ import React from "react";
 // Sections Imports
 import { Home, Works } from "../sections/index";
 
+import NewProjectSection from "../sections/NewProject/NewProject";
+
 const Landing = () => {
   return (
     <>
       <Home />
       <Works />
+      <NewProjectSection />
     </>
   );
 };
---
+++ src/sections/NewProject/NewProject.jsx
@@ -0,0 +1,46 @@
+//- Import React
+import React from 'react';
+import Countdown from 'react-countdown';
+
+//- Import Styles
+import styles from './NewProject.module.css';
+
+//- Import Images
+import Penguin from './assets/penguin.png';
+
+//- Import Components
+import Title from '../../components/title/title';
+import CtaButton from '../../components/Button/CtaButton/CtaButton';
+
+const NewProjectSection = () => {
+    return (
+        <section className={styles.Container}>
+             <div className={styles.Title}>
+                <Title text={"New project"}/>
+                <h3 className={styles.Soon}>COMING SOON</h3>
+            </div>
+            <div className={styles.Penguin}>
+                <img src={Penguin} />
+            </div>
+
+            <Countdown 
+                date={Date.now() + 200000000} 
+                renderer={props => (
+                    <div className={styles.Count}>
+                      {props.formatted.days} : {props.formatted.hours} : {props.formatted.minutes}
+                    </div>
+                  )}
+                zeroPadTime={2}
+            /> 
+            <div className={styles.CountInfo}>
+                    <a>DAYS &nbsp; &nbsp;</a> <a>HOURS</a> <a>MINUTES</a>
+            </div>
+
+            <div className={styles.Button}>
+                <CtaButton text="Start my project" onClick={"/"} />
+            </div>
+        </section>
+    );
+};
+    
+export default NewProjectSection;
