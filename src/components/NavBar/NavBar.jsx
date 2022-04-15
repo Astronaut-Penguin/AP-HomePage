12e4297d04b9a769fbf8eabfc308e8b91c546b74
Alejo Viola <viola.alejo@outlook.com>
2022-04-15 14:14:39 -0300

    [NavBar] - Creating NavBar component files

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+// REACT
+import React from "react";
+
+// STYLES
+import styles from "./NavBar.module.css";
+
+// COMPONENT
+const NavBar = () => {
+  ////////////
+  // RENDER //
+  ////////////
+  return <section className={styles.Container}></section>;
+};
+
+export default NavBar;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 // COMPONENTS EXPORT FILE
 
+// NAVBAR
+export { default as NavBar } from "./NavBar/NavBar";
+
 // BUTTONS
 export { default as CtaButton } from "./Button/CtaButton/CtaButton";
