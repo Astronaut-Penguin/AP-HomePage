0d13541d734cd24eda7bc13e16bc5036851ada74
Alejo Viola <viola.alejo@outlook.com>
2022-04-16 19:09:03 -0300

    [Us Section] - Fixing some ussues

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,12 +3,14 @@ import React from "react";
 
 // Sections Imports
 import { Home, Works } from "../sections/index";
+import UsSection from "../sections/Us/Us";
 
 const Landing = () => {
   return (
     <>
       <Home />
       <Works />
+      <UsSection />
     </>
   );
 };
