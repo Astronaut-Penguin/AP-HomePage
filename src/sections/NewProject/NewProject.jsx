dfee43727fad579b2d4f95e3cd54703d8f416a6f
Alejo Viola <viola.alejo@outlook.com>
2022-04-27 17:19:59 -0300

    [NewProject Section] - Adding Prittier and fixing some issues

--- src/sections/NewProject/NewProject.jsx
+++ src/sections/NewProject/NewProject.jsx
@@ -1,46 +1,47 @@
 //- Import React
-import React from 'react';
-import Countdown from 'react-countdown';
+import React from "react";
+import Countdown from "react-countdown";
 
 //- Import Styles
-import styles from './NewProject.module.css';
+import styles from "./NewProject.module.css";
 
 //- Import Images
-import Penguin from './assets/penguin.png';
+import Penguin from "./assets/penguin.png";
 
 //- Import Components
-import Title from '../../components/title/title';
-import CtaButton from '../../components/Button/CtaButton/CtaButton';
+import Title from "../../components/title/title";
+import CtaButton from "../../components/Button/CtaButton/CtaButton";
 
 const NewProjectSection = () => {
-    return (
-        <section className={styles.Container}>
-             <div className={styles.Title}>
-                <Title text={"New project"}/>
-                <h3 className={styles.Soon}>COMING SOON</h3>
-            </div>
-            <div className={styles.Penguin}>
-                <img src={Penguin} />
-            </div>
+  return (
+    <section className={styles.Container}>
+      <div className={styles.Title}>
+        <Title text={"New project"} />
+        <h3 className={styles.Soon}>COMING SOON</h3>
+      </div>
+      <div className={styles.Penguin}>
+        <img src={Penguin} />
+      </div>
 
-            <Countdown 
-                date={Date.now() + 200000000} 
-                renderer={props => (
-                    <div className={styles.Count}>
-                      {props.formatted.days} : {props.formatted.hours} : {props.formatted.minutes}
-                    </div>
-                  )}
-                zeroPadTime={2}
-            /> 
-            <div className={styles.CountInfo}>
-                    <a>DAYS &nbsp; &nbsp;</a> <a>HOURS</a> <a>MINUTES</a>
-            </div>
+      <Countdown
+        date={Date.now() + 200000000}
+        renderer={(props) => (
+          <div className={styles.Count}>
+            {props.formatted.days} : {props.formatted.hours} :{" "}
+            {props.formatted.minutes}
+          </div>
+        )}
+        zeroPadTime={2}
+      />
+      <div className={styles.CountInfo}>
+        <p>DAYS &nbsp; &nbsp;</p> <p>HOURS</p> <p>MINUTES</p>
+      </div>
 
-            <div className={styles.Button}>
-                <CtaButton text="Start my project" onClick={"/"} large />
-            </div>
-        </section>
-    );
+      <div className={styles.Button}>
+        <CtaButton text="Start my project" onClick={"/"} large />
+      </div>
+    </section>
+  );
 };
-    
+
 export default NewProjectSection;
